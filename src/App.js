323f5360369c95842bc6f7d8c4c005aaa8568511
rjaybernardo <raphaeljaybernardo@gmail.com>
2022-09-20 21:46:44 +0800

    main component fetch movies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
 
 export default function App() {
   return (
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,7 +1,17 @@
 import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+import requests from './../../Requests'
 
 const Main = () => {
   const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    axios.get(requests.requestPopular).then((response) => {
+      setMovies(response.data.results)
+    })
+  }, [])
+  console.log(movies)
 
   return <div>Main</div>
 }
