13f9dc015854eef32b0c3ca328b5ef74714c76a3
rjaybernardo <raphaeljaybernardo@gmail.com>
2022-09-20 23:23:21 +0800

    setup AuthContextProvider from AuthContext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
+import { AuthContextProvider } from './context/AuthContext'
 
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
@@ -6,10 +7,12 @@ import Home from './pages/Home'
 export default function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </AuthContextProvider>
     </>
   )
 }
