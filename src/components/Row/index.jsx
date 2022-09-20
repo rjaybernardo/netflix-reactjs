4666363437b16a1a16cd7948d971ee9cb1210c2b
rjaybernardo <raphaeljaybernardo@gmail.com>
2022-09-20 22:24:43 +0800

    create Movie component to pass to Row component

--- src/components/Row/index.jsx
+++ src/components/Row/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 import { FaHeart, FaRegHeart } from 'react-icons/fa'
+import Movie from '../Movie/index'
 
 const Row = ({ title, fetchURL, rowID }) => {
   const [movies, setMovies] = useState([])
@@ -24,28 +25,7 @@ const Row = ({ title, fetchURL, rowID }) => {
           className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'
         >
           {movies.map((item, id) => (
-            <div
-              key={id}
-              className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'
-            >
-              <img
-                className='w-full h-auto block'
-                src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
-                alt={item?.title}
-              />
-              <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
-                <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>
-                  {item?.title}
-                </p>
-                <p>
-                  {like ? (
-                    <FaHeart className='absolute top-4 left-4 text-gray-300' />
-                  ) : (
-                    <FaRegHeart className='absolute top-4 left-4 text-gray-300' />
-                  )}
-                </p>
-              </div>
-            </div>
+            <Movie key={id} item={item} />
           ))}
         </div>
       </div>
