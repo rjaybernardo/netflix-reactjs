29b93f1592f26f6b2806387507ac0c3f3c351db8
rjaybernardo <raphaeljaybernardo@gmail.com>
2022-09-20 21:33:04 +0800

    Request.js file and .env for API key

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { Route, Routes } from 'react-router-dom'
+
 import Navbar from './components/Navbar'
+
 export default function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </>
   )
 }
